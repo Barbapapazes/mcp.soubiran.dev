92bc6f65d52e984fca67cf4cc7117c4a366d62bc
barbapapazes <e.soubiran25@gmail.com>
2025-05-11 10:26:04 +0200

    build: publish to npm

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { Server } from "@modelcontextprotocol/sdk/server/index.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { Server } from '@modelcontextprotocol/sdk/server/index.js'
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import {
   CallToolRequestSchema,
-  ListToolsRequestSchema
-} from "@modelcontextprotocol/sdk/types.js";
+  ListToolsRequestSchema,
+} from '@modelcontextprotocol/sdk/types.js'
 import { ofetch } from 'ofetch'
-import { z } from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
+import { z } from 'zod'
+import { zodToJsonSchema } from 'zod-to-json-schema'
 
 const server = new Server(
   {
-    name: "mcp.soubiran.dev",
-    version: "0.0.0"
+    name: 'mcp.soubiran.dev',
+    version: '0.0.0',
   },
   {
     capabilities: {
-      tools: {}
-    }
-  }
-);
+      tools: {},
+    },
+  },
+)
 
 const GetNextId = z.object({})
 
@@ -26,33 +26,34 @@ server.setRequestHandler(ListToolsRequestSchema, async () => {
   return {
     tools: [
       {
-        name: "next_post_id",
-        description: "Get the next post ID for a post on soubiran.dev",
+        name: 'next_post_id',
+        description: 'Get the next post ID for a post on soubiran.dev',
         inputSchema: zodToJsonSchema(GetNextId),
       },
-    ]
-  };
-});
+    ],
+  }
+})
 
 server.setRequestHandler(CallToolRequestSchema, async (request) => {
-  if (request.params.name === "next_post_id") {
+  if (request.params.name === 'next_post_id') {
     try {
-      const data = await ofetch<{ next_post_id: number }>('http://localhost:8000/api/posts/next-id');
+      const data = await ofetch<{ next_post_id: number }>('http://localhost:8000/api/posts/next-id')
 
       return {
         content: [{
           type: 'text',
-          text: data.next_post_id
-        }]
+          text: data.next_post_id,
+        }],
       }
     }
     catch (error) {
-      throw new Error("Error fetching next post ID");
+      throw new Error(`Error fetching next post ID: ${error}`)
     }
   }
 
-  throw new Error("Unknown tool");
-});
+  throw new Error('Unknown tool')
+})
 
-const transport = new StdioServerTransport();
-await server.connect(transport);
+const transport = new StdioServerTransport()
+// eslint-disable-next-line antfu/no-top-level-await
+await server.connect(transport)
